8f7c1d3eb449daaac50aafc08c2af0d487a4e94b
YNS <younesbenkheil@gmail.com>
2020-11-20 18:00:30 +0100

    commit

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+/*
+Plugin Name: Carsync
+Plugin URI: https://github.com/younesben99/carsync
+Description: Autoscout24 sync
+Version: 0.1
+Author: Younes Benkheil
+Author URI: https://digiflow.be/
+License: GPL2
+*/
 
-echo "test"
+echo "test";
 
 ?>
