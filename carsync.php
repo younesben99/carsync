e43f440f72fd9d062e39c569c35c8c1f6446e473
YNS <younesbenkheil@gmail.com>
2020-11-20 20:00:41 +0100

    commit

--- carsync.php
+++ carsync.php
@@ -3,7 +3,7 @@
 Plugin Name: Carsync
 Plugin URI: https://github.com/younesben99/carsync
 Description: Autoscout24 sync
-Version: 0.1
+Version: 0.2
 Author: Younes Benkheil
 Author URI: https://digiflow.be/
 License: GPL2
