137a0c8cb9d3f219ebc25e31127a4add20daf4a6
YNS <younesbenkheil@gmail.com>
2020-11-20 18:42:01 +0100

    commit

--- carsync.php
+++ carsync.php
@@ -1,15 +1,14 @@
 <?php
-/*
-Plugin Name: Carsync
-Plugin URI: https://github.com/younesben99/carsync
-Description: Autoscout24 sync
-Version: 0.1
-Author: Younes Benkheil
-Author URI: https://digiflow.be/
-License: GPL2
-GitHub Plugin URI: https://github.com/younesben99/carsync
-*/
-
+/**
+ * Plugin Name:       Carsync   
+ * Plugin URI:        https://github.com/younesben99/carsync
+ * Description:       Autoscout sync with Wordpress
+ * Version:           0.1
+ * Author:            Younes Benkheil
+ * License:           GNU General Public License v2
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
+ * GitHub Plugin URI: https://github.com/younesben99/carsync
+ */
 echo "test";
 
 ?>
