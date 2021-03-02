252a9a4c0a8faee4dfc0bc56aa9d94f2737189e3
Younes Benkheil <younesbenkheil@gmail.com>
2021-03-02 11:47:08 +0100

    cors fix

--- carsync.php
+++ carsync.php
@@ -4,7 +4,7 @@
 Plugin Name: Digiflow Carsync
 Plugin URI: https://github.com/younesben99/carsync
 Description: A plugin that syncs autoscout24 cars with wordpress posts.
-Version: 2.6
+Version: 2.65
 Author: Younes Benkheil
 Author URI: https://digiflow.be/
 License: GPL2
