afa91681d8fee852c2cedde0c127b11e7fede7fc
Younes Benkheil <younesbenkheil@gmail.com>
2021-09-23 10:30:54 +0200

    server overload

--- sync/carsync_image_handler.php
+++ sync/carsync_image_handler.php
@@ -35,64 +35,68 @@ function dds_car_fotos_downloaden(){
     //$current_post = $posts_arr[0];
 
 
+    $count = 0;
     foreach($posts_arr as $current_post){
 
-        if(!empty($current_post)){
-            //echo "id: " . $current_post;
+        if ($count < 10) {
+            if (!empty($current_post)) {
+                //echo "id: " . $current_post;
             
-            $local_gallery = get_post_meta( $current_post,"vdw_gallery_id" );
-            var_dump($local_gallery);
+                $local_gallery = get_post_meta($current_post, "vdw_gallery_id");
+                var_dump($local_gallery);
             
-            $api_gallery = get_post_meta( $current_post,"_car_syncimages_key" );
-            var_dump($api_gallery);
-            $imgtoadd = array();
+                $api_gallery = get_post_meta($current_post, "_car_syncimages_key");
+                var_dump($api_gallery);
+                $imgtoadd = array();
     
-            if(!empty($api_gallery)){
-                foreach($api_gallery[0] as $img){
-                    $image = "";
-                    if($img != "") {
-                    
-                        $file = array();
-                        $file['name'] = $img;
-                        $file['tmp_name'] = download_url($img);
+                if (!empty($api_gallery)) {
+                    foreach ($api_gallery[0] as $img) {
+                        $image = "";
+                        if ($img != "") {
+                            $file = array();
+                            $file['name'] = $img;
+                            $file['tmp_name'] = download_url($img);
                 
-                        if (is_wp_error($file['tmp_name'])) {
-                            @unlink($file['tmp_name']);
-                            var_dump( $file['tmp_name']->get_error_messages( ) );
-                            array_push($imgtoadd,1);
-    
-                        } else {
-                            var_dump($file['tmp_name']);
-                            
-                            $file['name'] = slugify(strtolower(get_the_title($current_post))) . "_" .uniqid().".jpg";
-                            var_dump($file['name']);
-                            $attachmentId = media_handle_sideload($file, $post_id);
-                            
-                            if ( is_wp_error($attachmentId) ) {
+                            if (is_wp_error($file['tmp_name'])) {
                                 @unlink($file['tmp_name']);
-                                var_dump( $attachmentId->get_error_messages( ) );
-                                array_push($imgtoadd,1);
-                            } else {                
-                                $image = wp_get_attachment_url( $attachmentId );
-                                array_push($imgtoadd,$attachmentId);
+                                var_dump($file['tmp_name']->get_error_messages());
+                                array_push($imgtoadd, 1);
+                            } else {
+                                var_dump($file['tmp_name']);
+                            
+                                $file['name'] = slugify(strtolower(get_the_title($current_post))) . "_" .uniqid().".jpg";
+                                var_dump($file['name']);
+                                $attachmentId = media_handle_sideload($file, $post_id);
+                            
+                                if (is_wp_error($attachmentId)) {
+                                    @unlink($file['tmp_name']);
+                                    var_dump($attachmentId->get_error_messages());
+                                    array_push($imgtoadd, 1);
+                                } else {
+                                    $image = wp_get_attachment_url($attachmentId);
+                                    array_push($imgtoadd, $attachmentId);
+                                }
                             }
                         }
                     }
+                    update_post_meta($current_post, '_car_api_images_downloaded', "YES");
                 }
-                update_post_meta($current_post, '_car_api_images_downloaded', "YES");
-            }
     
-            if(!empty($imgtoadd)){
-                update_post_meta($current_post, 'vdw_gallery_id', $imgtoadd);
-            }
+                if (!empty($imgtoadd)) {
+                    update_post_meta($current_post, 'vdw_gallery_id', $imgtoadd);
+                }
     
             
     
-            // echo($current_post);
+                // echo($current_post);
             // echo("<br>");
             // echo get_the_title($current_post);
-    
+            }
         }
+        else{
+            return;
+        }
+        $count++;
     }
 
 
