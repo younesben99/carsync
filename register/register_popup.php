c53ec020d42d4be288710ecf7755ebe83ed16376
Younes <info@digiflow.be>
2022-07-25 02:16:22 +0200

    header bug fix

--- register/register_popup.php
+++ register/register_popup.php
@@ -1,7 +1,4 @@
-
 <?php
-
-
 function popup_open($popup_name){
 ?>
 
@@ -9,7 +6,6 @@ function popup_open($popup_name){
 <div class="dds_pop_inner">
 <div class="dds_pop_close"><span>&#x2715</span> <span>Sluiten</span></div>
 <div class="dds_pop_content">
-
 <?php
 }
 function popup_close(){
@@ -20,14 +16,4 @@ function popup_close(){
     </div>
     <?php
 }
-
-
 ?>
-
-
-
-
-
-
-
-
