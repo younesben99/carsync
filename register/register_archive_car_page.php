212416eed35c77df793f548c4cc029b7944d21bb
Younes <info@digiflow.be>
2023-10-16 15:02:58 +0200

    update ad spend

--- register/register_archive_car_page.php
+++ register/register_archive_car_page.php
@@ -8,7 +8,7 @@ if(!file_exists(get_template_directory() . "/archive-autos.php")){
     fclose($myfile);
 }
 
-if(file_exists(get_template_directory() . "/adspend-taxonomy-template.php")){
+if(!file_exists(get_template_directory() . "/adspend-taxonomy-template.php")){
     $myfile = fopen(get_template_directory()  . "/adspend-taxonomy-template.php", "w");
     $content = '<?php
     include(__DIR__."/../../plugins/carsync/templates/adspend-taxonomy-template.php");
