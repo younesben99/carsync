e1c6725fb06bee4f156e91c5471ca8b21a4a53a8
Younes <info@digiflow.be>
2023-12-18 15:18:33 +0100

    update sandbox

--- sync/sb.php
+++ sync/sb.php
@@ -2,18 +2,18 @@
 include(__DIR__."/../../../../wp-load.php");
 
 
-
+carsync_posts_maken();
 
 
 ?>
 
 
-<form action="https://digiflowlocal.local/wp-content/plugins/carsync/php/ajax_zapier.php" method="post">
+<!-- <form action="https://digiflowlocal.local/wp-content/plugins/carsync/php/ajax_zapier.php" method="post">
 
 <input type="text" name="offerte_url" value="testurl">
 <input type="text" name="offerte_id" value="testid">
 <input type="text" name="vin" value="WVWZZZ3CZCE708301">
 
 <input type="submit">
-</form>
+</form> -->
 
